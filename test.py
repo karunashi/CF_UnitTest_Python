482bb1e27b117b9467370f9c55ede45639271109
karunashi <karunashi@gmail.com>
2017-09-08 21:41:24 -0700

    Added initial environment for unit testing. Moved over to Sublime after installing necessary packages (installed Anaconda for build) after using Spyder for a bit.

--- test.py
+++ test.py
@@ -2,8 +2,21 @@
 """
 Created on Fri Sep  8 20:46:28 2017
 
-@author: karun
+@author: karunashi
+Initially worked on via Spyder 3.6 IDE, then moved back to Sublime 3
 """
 
 import unittest
+from sample_account import Customer
 
+class Test(unittest.TestCase):
+    def setUp(self):
+        """ Setting up initial values for the test case """
+        Customer.set_balance(self, 50)
+
+    def tearDown(self):
+    	""" Ending test case"""
+        return
+
+if __name__ == "__main__":
+    unittest.main()
